597ff1fd7761d6552078706fa21a4a9e141c2469
Ekaterina Shiverskikh <katerina_kononenko_94@mai.ru>
2023-09-06 23:56:44 +0700

    added background

--- script/main.js
+++ script/main.js
@@ -1,7 +1,13 @@
 
+
 $('.single-item').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-  });
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+
+
