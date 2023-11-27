94e1a43dd12697952afe5af6bd8925268a64ad74
Ekaterina <katerina_kononenko_94@mail.ru>
2023-11-27 21:04:50 +0700

    added counter

--- script/main.js
+++ script/main.js
@@ -10,4 +10,19 @@ $('.single-item').slick({
   autoplaySpeed: 3000,
 });
 
+// Получаем текущее количество просмотров из local storage
+let views = localStorage.getItem('pageViews');
+
+// Проверяем, есть ли уже значение в local storage
+if (views) {
+  views = parseInt(views);
+  views++;  // Увеличиваем количество просмотров
+  localStorage.setItem('pageViews', views);  // Обновляем значение в local storage
+} else {
+  views = 1;
+  localStorage.setItem('pageViews', views);  // Устанавливаем начальное значение в local storage
+}
+
+// Отображаем количество просмотров на странице
+console.log('Количество просмотров: ' + views);
 
